10a248c00caacb38bb548048229245203847ec7f
jcleira <jmc.leira@gmail.com>
2023-11-13 17:11:10 +0100

    Add .gitignore

--- exercise-01/target/debug/deps/exercise_01-21ac79c21c97be19.d
+++
@@ -1,5 +0,0 @@
-/Users/arvos/Code/rust/src/github.com/jcleira/comprehensive-rust/exercise-01/target/debug/deps/exercise_01-21ac79c21c97be19: src/main.rs
-
-/Users/arvos/Code/rust/src/github.com/jcleira/comprehensive-rust/exercise-01/target/debug/deps/exercise_01-21ac79c21c97be19.d: src/main.rs
-
-src/main.rs:
--- exercise-01/target/debug/deps/exercise_01-60ead0121b703fc1.d
+++
@@ -1,5 +0,0 @@
-/Users/arvos/Code/rust/src/github.com/jcleira/comprehensive-rust/exercise-01/target/debug/deps/exercise_01-60ead0121b703fc1.rmeta: src/main.rs
-
-/Users/arvos/Code/rust/src/github.com/jcleira/comprehensive-rust/exercise-01/target/debug/deps/exercise_01-60ead0121b703fc1.d: src/main.rs
-
-src/main.rs:
