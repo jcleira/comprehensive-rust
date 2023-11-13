6be494ff65005fb31a1a7a09c7ffbe25c9efa2cb
jcleira <jmc.leira@gmail.com>
2023-11-13 17:08:21 +0100

    Add exercise-01

---
+++ exercise-01/target/debug/deps/exercise_01-21ac79c21c97be19.d
@@ -0,0 +1,5 @@
+/Users/arvos/Code/rust/src/github.com/jcleira/comprehensive-rust/exercise-01/target/debug/deps/exercise_01-21ac79c21c97be19: src/main.rs
+
+/Users/arvos/Code/rust/src/github.com/jcleira/comprehensive-rust/exercise-01/target/debug/deps/exercise_01-21ac79c21c97be19.d: src/main.rs
+
+src/main.rs:
---
+++ exercise-01/target/debug/exercise-01.d
@@ -0,0 +1,1 @@
+/Users/arvos/Code/rust/src/github.com/jcleira/comprehensive-rust/exercise-01/target/debug/exercise-01: /Users/arvos/Code/rust/src/github.com/jcleira/comprehensive-rust/exercise-01/src/main.rs
