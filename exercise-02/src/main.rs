81ad06529bc00b8ccd94feeedcfad30e57cc54cd
jcleira <jmc.leira@gmail.com>
2023-11-13 17:43:03 +0100

    Add exercise-02

---
+++ exercise-02/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    let mut x: i32 = 6;
+    print!("x = {x}");
+
+    while x != 1 {
+        if x % 2 == 0 {
+            x = x / 2;
+        } else {
+            x = 3 * x + 1;
+        }
+
+        print!(" -> {x}");
+    }
+
+    println!();
+}
